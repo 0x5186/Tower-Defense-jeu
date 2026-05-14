ea74b6ec38c9cdc4d79bcadf10a41dc18fe5d208
Nassim-chaieb <chaiebnassim14@gmail.com>
2026-05-14 20:47:45 +0200

    avancement sur le tile map et son design ajout d'un menu et d'un bouton qui ramene sur fenetreJeu.fxml ajout de nouveaux sprite

--- src/main/java/universite_paris8/iut/nchaieb/sae_jeux/modele/Terrain.java
+++ src/main/java/universite_paris8/iut/nchaieb/sae_jeux/modele/Terrain.java
@@ -1,12 +1,77 @@
 package universite_paris8.iut.nchaieb.sae_jeux.modele;
 
+import java.util.jar.JarEntry;
+
 public class Terrain {
 
-    private int[][] codeTuiles = {{1,1,1},{2,1,2},{3,3,3},{3,3,3}};
+    private int[][] codeTuiles= new int[25][48];
+
+    public void terrainPlainesCode(){
+        for(int i=0; i<6; i++){
+            for(int j=0; j<48; j++) {
+                codeTuiles[i][j] = 1;
+            }
+        }
+        for(int j=0; j<48; j++) {
+            codeTuiles[6][j] = 5;
+        }
+
+
+
+
+        for(int i=7; i<16; i++){
+            for(int j=0; j<48; j++) {
+                codeTuiles[i][j] = 3;
+            }
+        }
+        for(int i=0; i<19; i++){
+            int iAleatoire = (int)(Math.random() * 4);
+            int jAleatoire = (int)(Math.random() * 48);
+            codeTuiles[iAleatoire][jAleatoire] = 2;
+
+        }
+        for(int i=0; i<25; i++) {
+            int iAleatoire = (int) ((Math.random() * 9)+7);
+            int jAleatoire = (int) (Math.random() * 48);
+            codeTuiles[iAleatoire][jAleatoire] = 4;
+        }
+
+        for(int i=11; i<13; i++){
+            for(int j=0; j<48; j++) {
+                codeTuiles[i][j] = 6;
+            }
+        }
+        for(int j=0; j<48; j++) {
+            codeTuiles[10][j] = 7;
+        }
+        for(int j=0; j<48; j++) {
+            codeTuiles[13][j] = 8;
+        }
+        for(int i=16; i<24; i++){
+            for(int j=0; j<48; j++) {
+                codeTuiles[i][j] = 9;
+            }
+        }
+        for(int j=0; j<48; j++) {
+            codeTuiles[16][j] = 10;
+        }
+
+
+
+    }
 
 
 
     public int hauteur() {return this.codeTuiles.length;}
     public int largeur() {return this.codeTuiles[0].length;}
     public int codeTuile(int ligne, int col) {return codeTuiles[ligne][col];}
+
+    public void test() {
+
+        for(int i=0; i<25; i++){
+            for(int j=0; j<48; j++) {
+                codeTuiles[i][j] = 1;
+            }
+        }
+    }
 }
--- src/main/java/universite_paris8/iut/nchaieb/sae_jeux/vue/TerrainVue.java
+++ src/main/java/universite_paris8/iut/nchaieb/sae_jeux/vue/TerrainVue.java
@@ -1,30 +1,52 @@
 package universite_paris8.iut.nchaieb.sae_jeux.vue;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import universite_paris8.iut.nchaieb.sae_jeux.modele.Terrain;
+import universite_paris8.iut.nchaieb.sae_jeux.Main;
 
 public class TerrainVue {
 
-    @FXML
+
+
+    Image bleu = new Image(Main.class.getResourceAsStream("images/bleu.png"));
+
+    Image  herbe = new Image(Main.class.getResourceAsStream("images/herbe.png"));
+    Image  tableau = new Image(Main.class.getResourceAsStream("images/tableau.png"));
+    Image  nuage = new Image(Main.class.getResourceAsStream("images/bleunuage.png"));
+    Image  herbecailloux = new Image(Main.class.getResourceAsStream("images/herbecailloux.png"));
+    Image  barriere = new Image(Main.class.getResourceAsStream("images/barriere.png"));
+    Image  solmilieu = new Image(Main.class.getResourceAsStream("images/solmilieu.png"));
+    Image  solhaut = new Image(Main.class.getResourceAsStream("images/solhaut.png"));
+    Image  solbas = new Image(Main.class.getResourceAsStream("images/solbas.png"));
+    Image  tableauBase = new Image(Main.class.getResourceAsStream("images/tableauBase.png"));
+    Image  tableauHaut1 = new Image(Main.class.getResourceAsStream("images/tableauHaut1.png"));
+    Image  tableauHaut2 = new Image(Main.class.getResourceAsStream("images/tableauHaut2.png"));
+    Image  test = new Image(Main.class.getResourceAsStream("images/test.png"));
     private TilePane tilePane;
-
-    Image bleu = new Image(getClass().getResourceAsStream("images/bleu.png"));
-    Image  marron = new Image(getClass().getResourceAsStream("images/marron.png"));
-    Image  vert = new Image(getClass().getResourceAsStream("images/vert.png"));
-    Image  tableau = new Image(getClass().getResourceAsStream("images/tableau.png"));
-    Image  nuage = new Image(getClass().getResourceAsStream("images/bleunuage.png"));
-
     private Terrain terrain;
 
-    public TerrainVue(Terrain terrain) {
+    public TerrainVue(Terrain terrain, TilePane tilePane) {
         this.terrain = terrain;
+        this.tilePane = tilePane;
     }
-    public void dessine(){
-        for (int l = 0 ; l<this.terrain.hauteur() ; l++)
-            for (int col = 0 ; col < this.terrain.largeur() ; col++) {
+    public void dessine(int map) {
+        terrain.test();
+        switch (map){
+            case 1:
+                terrain.test();
+                break;
+
+            case 2:
+                terrain.terrainPlainesCode();
+                break;
+        }
+
+        for (int l = 0; l < this.terrain.hauteur(); l++) {
+            for (int col = 0; col < this.terrain.largeur(); col++) {
                 switch (this.terrain.codeTuile(l, col)) {
                     case 1:
                         ImageView ciel = new ImageView(bleu);
@@ -36,10 +58,48 @@ public class TerrainVue {
                         this.tilePane.getChildren().add(nuagebleu);
                         break;
                     case 3:
-                        ImageView solHerbe = new ImageView(vert);
+                        ImageView solHerbe = new ImageView(herbe);
                         this.tilePane.getChildren().add(solHerbe);
+                        break;
+                    case 4:
+                        ImageView herberavecailloux = new ImageView(herbecailloux);
+                        this.tilePane.getChildren().add(herberavecailloux);
+                        break;
+                    case 5:
+                        ImageView barrieres = new ImageView(barriere);
+                        this.tilePane.getChildren().add(barrieres);
+                        break;
+                    case 6:
+                        ImageView solTerreMilieu = new ImageView(solmilieu);
+                        this.tilePane.getChildren().add(solTerreMilieu);
+                        break;
+                    case 7:
+                        ImageView solTerreHaut = new ImageView(solhaut);
+                        this.tilePane.getChildren().add(solTerreHaut);
+                        break;
+                    case 8:
+                        ImageView solTerreBas = new ImageView(solbas);
+                        this.tilePane.getChildren().add(solTerreBas);
+                        break;
+                    case 9:
+                        ImageView tabBase = new ImageView(tableauBase);
+                        this.tilePane.getChildren().add(tabBase);
+                        break;
+                    case 10:
+                        ImageView tabHaut1 = new ImageView(tableauHaut1);
+                        this.tilePane.getChildren().add(tabHaut1);
+                        break;
+                    case 11:
+                        ImageView tabHaut2 = new ImageView(tableauHaut2);
+                        this.tilePane.getChildren().add(tabHaut2);
+                        break;
+                    case 12:
+                        ImageView test1 = new ImageView(test);
+                        this.tilePane.getChildren().add(test1);
+                        break;
 
                 }
             }
+        }
     }
 }
